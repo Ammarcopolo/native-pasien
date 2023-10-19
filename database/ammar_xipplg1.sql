772f1e343091efbdf3db9175efb11a01c056a86b
Ammarcopolo <144409653+Ammarcopolo@users.noreply.github.com>
2023-10-19 13:14:12 +0700

    Add files via upload

---
+++ database/ammar_xipplg1.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Inang: 127.0.0.1
+-- Waktu pembuatan: 19 Okt 2023 pada 08.13
+-- Versi Server: 5.5.27
+-- Versi PHP: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Basis data: `ammar_xipplg1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pasien`
+--
+
+CREATE TABLE IF NOT EXISTS `pasien` (
+  `idPasien` varchar(50) NOT NULL,
+  `nmPasien` varchar(50) NOT NULL,
+  `jk` char(15) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  PRIMARY KEY (`idPasien`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `pasien`
+--
+
+INSERT INTO `pasien` (`idPasien`, `nmPasien`, `jk`, `alamat`) VALUES
+('3213', 'yantok', '', 'assa');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
